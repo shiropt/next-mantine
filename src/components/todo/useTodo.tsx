8029ce2c73b115227d45acdacd5938b7a9b34d94
qin-todo-team-j4 <sh071332@gmail.com>
2022-06-19 18:50:24 +0900

    husky,lint-staged導入

--- src/components/todo/useTodo.tsx
+++ src/components/todo/useTodo.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useState } from "react";
 import { mutate } from "swr";
 import {
   deleteData,
