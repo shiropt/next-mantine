4aa791f5b211d6221728d1818bf0181f67f1a0eb
qin-todo-team-j4 <sh071332@gmail.com>
2022-06-20 08:55:01 +0900

    RTL導入

--- src/components/ErrorBoundary.tsx
+++ src/components/ErrorBoundary.tsx
@@ -1,7 +1,7 @@
 import { Stack, Text } from "@mantine/core";
 import { FallbackProps } from "react-error-boundary";
 
-export const ErrorFallback = ({ error, resetErrorBoundary }: FallbackProps) => {
+export const ErrorFallback = ({ error }: FallbackProps) => {
   return (
     <Stack align="center" justify="center" className="h-screen" role="alert">
       <Text className=" font-bold">{error.message}</Text>
